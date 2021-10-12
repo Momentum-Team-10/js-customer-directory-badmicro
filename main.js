7cd08977840ac7574a62b907fac622a7958e41ba
Jeffrey <jrgilley92@gmail.com>
2021-10-12 14:49:27 -0400

    remove css styles, add library UIKit

---
+++ main.js
@@ -0,0 +1,9 @@
+
+const customer = customers[0]
+
+const root = document.getElementById('root')
+const container = document.createElement('div')
+root.appendChild(container)
+
+
+const customerCard = document.createElement('div')
