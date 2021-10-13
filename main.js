5eba8ebc78eca64487c3d7d113732718426b6e7e
Jeffrey <jrgilley92@gmail.com>
2021-10-13 09:20:19 -0400

    not working, unsure.

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
-
-const customer = customers[0]
-
-const root = document.getElementById('root')
-const container = document.createElement('div')
-root.appendChild(container)
+const root = document.getElementById("root");
+const container = document.createElement("div");
+root.appendChild(container);
+container.classList.add("uk-container", "uk-flex", "uk-flex-between");
 
 
-const customerCard = document.createElement('div')
+for (let customer of customers) {
+  const customerCard = document.createElement("div");
+  customerCard.id = `${customer.id.value}`;
+  container.appendChild(customerCard)
+  customerCard.classList.add(
+      "uk-card", "uk-card-body", "uk-card-default", "uk-width-medium");
+
+
+  customerCard.innerHTML = `
+    <img src=${customer.picture.medium} />
+    <p>${capsName(customer.name.first) + capsName(customer.name.last)}</p>
+    <p>${customer.email}</p>
+    <p>${toLocationSting(customer)}</p>
+    <p>DOB: ${moment(customer.dob.date)}</p>
+    <p>Customer since: ${moment(customer.registered.date)}</p>
+    `;
+}
+
+function capsName(name) {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+}
+
+function toLocationSting(customer) {
+  let locationString = "";
+  locationString +=
+    customer.location.street.number +
+    " " +
+    customer.location.street.name +
+    "\n";
+  locationString +=
+    customer.location.city +
+    ", " +
+    nameToAbbr(customer.location.state) +
+    customer.location.postcode;
+  return locationString;
+}
